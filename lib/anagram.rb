847022dcc3c1243a1d4736217b2127a1021502b3
Sav Younts <savyounts@gmail.com>
2018-08-08 18:41:51 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(possible_anagrams)
+    possible_anagrams.select do |possible_anagram|
+      possible_anagram.split("").sort == @word.split("").sort
+    end
+  end
+  
+end
